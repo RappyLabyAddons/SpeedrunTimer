f2c9b8471ce8e0c20acc84d81f58c6a5f346be98
RappyTV <rappytv@rappytv.com>
2024-09-26 23:42:31 +0200

    Add config icons

--- core/src/main/java/com/rappytv/speedruntimer/SpeedrunTimerConfig.java
+++ core/src/main/java/com/rappytv/speedruntimer/SpeedrunTimerConfig.java
@@ -2,13 +2,18 @@ package com.rappytv.speedruntimer;
 
 import net.labymod.api.addon.AddonConfig;
 import net.labymod.api.client.gui.screen.widget.widgets.input.SwitchWidget.SwitchSetting;
+import net.labymod.api.configuration.loader.annotation.SpriteSlot;
+import net.labymod.api.configuration.loader.annotation.SpriteTexture;
 import net.labymod.api.configuration.loader.property.ConfigProperty;
 
+@SpriteTexture("settings")
 public class SpeedrunTimerConfig extends AddonConfig {
 
+    @SpriteSlot(size = 32)
     @SwitchSetting
     private final ConfigProperty<Boolean> enabled = new ConfigProperty<>(true);
 
+    @SpriteSlot(size = 32, x = 1)
     @SwitchSetting
     private final ConfigProperty<Boolean> countdownSound = new ConfigProperty<>(true);
 
