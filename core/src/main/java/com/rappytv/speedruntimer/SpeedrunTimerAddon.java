ac8913c3bd5bc777f8bf3540d7a8e807b95c558b
RappyTV <rappytv@rappytv.com>
2024-09-26 22:08:17 +0200

    Register command and hudWidget

--- core/src/main/java/com/rappytv/speedruntimer/SpeedrunTimerAddon.java
+++ core/src/main/java/com/rappytv/speedruntimer/SpeedrunTimerAddon.java
@@ -1,18 +1,30 @@
 package com.rappytv.speedruntimer;
 
+import com.rappytv.speedruntimer.command.TimerCommand;
+import com.rappytv.speedruntimer.hudWidget.TimerHudWidget;
+import com.rappytv.speedruntimer.util.Timer;
+import net.labymod.api.Laby;
 import net.labymod.api.addon.LabyAddon;
 import net.labymod.api.models.addon.annotation.AddonMain;
 
 @AddonMain
 public class SpeedrunTimerAddon extends LabyAddon<SpeedrunTimerConfig> {
 
+    private final Timer timer = new Timer();
+
     @Override
     protected void enable() {
         registerSettingCategory();
+        registerCommand(new TimerCommand(this));
+        Laby.labyAPI().hudWidgetRegistry().register(new TimerHudWidget(this));
     }
 
     @Override
     protected Class<? extends SpeedrunTimerConfig> configurationClass() {
         return SpeedrunTimerConfig.class;
+    }
+
+    public Timer getTimer() {
+        return timer;
     }
 }
