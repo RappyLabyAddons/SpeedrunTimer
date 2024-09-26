ead3b220038ff418cee737fdc8ea3a510da5250e
RappyTV <rappytv@rappytv.com>
2024-09-26 22:07:19 +0200

    Add timer display hudWidget

---
+++ core/src/main/java/com/rappytv/speedruntimer/hudWidget/TimerHudWidget.java
@@ -0,0 +1,37 @@
+package com.rappytv.speedruntimer.hudWidget;
+
+import com.rappytv.speedruntimer.SpeedrunTimerAddon;
+import com.rappytv.speedruntimer.util.Timer.TimerState;
+import net.labymod.api.Laby;
+import net.labymod.api.client.gui.hud.hudwidget.HudWidgetConfig;
+import net.labymod.api.client.gui.hud.hudwidget.SimpleHudWidget;
+import net.labymod.api.client.gui.hud.position.HudSize;
+import net.labymod.api.client.gui.mouse.MutableMouse;
+import net.labymod.api.client.render.font.ComponentRenderer;
+import net.labymod.api.client.render.font.RenderableComponent;
+import net.labymod.api.client.render.matrix.Stack;
+
+public class TimerHudWidget extends SimpleHudWidget<HudWidgetConfig> {
+
+    private final SpeedrunTimerAddon addon;
+
+    public TimerHudWidget(SpeedrunTimerAddon addon) {
+        super("speedruntimer_display", HudWidgetConfig.class);
+        this.addon = addon;
+    }
+
+    @Override
+    public void render(Stack stack, MutableMouse mouse, float partialTicks, boolean isEditorContext, HudSize size) {
+        ComponentRenderer componentRenderer = Laby.references().renderPipeline().componentRenderer();
+        RenderableComponent statusComponent = RenderableComponent.of(addon.getTimer().getDisplay());
+        if (stack != null) {
+            componentRenderer.builder().text(statusComponent).pos(this.anchor.isLeft() ? 2 : (this.anchor.isCenter() ? statusComponent.getWidth() / 2.0f : 2.0f), 0).color(-1).shadow(true).centered(this.anchor.isCenter()).render(stack);
+        }
+        size.set(statusComponent.getWidth(), statusComponent.getHeight());
+    }
+
+    @Override
+    public boolean isVisibleInGame() {
+        return addon.getTimer().getState() != TimerState.OFF;
+    }
+}
